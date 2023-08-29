2441d1e634424fe455733076f0262a056d2c94b2
MAKCIMELIANO <makcimeliano@gmail.com>
2023-08-29 16:52:53 +0300

    player

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import { Example1 } from './Example1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Example1 />
   </React.StrictMode>,
 );
